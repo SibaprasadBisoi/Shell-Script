0f1d9f9148edab8611cee1f27e1ac451507ab05f
root <root@ip-172-31-14-152.ap-south-1.compute.internal>
2023-08-04 02:19:14 +0000

    first checkin for the repository

---
+++ first.sh
@@ -0,0 +1,21 @@
+echo "i will be a great devops engineer"
+
+echo $BASH
+
+name="Siva"
+
+echo "hello ${name}", please enter your age.
+
+read  age
+
+echo "my age is ${age}"
+
+echo  "Sub: hallo, i am $1"
+
+sleep 5 
+
+echo "welcome to the devops world"
+
+sleep 5
+
+echo "we are here for hands on experience"
